02b7cfea863a922ee2620a97203e9042437db433
Emon Hossain <emondd4@gmail.com>
2022-02-01 00:31:14 +0600

    bump 2

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,9 +1,10 @@
+import 'dart:convert' as convert;
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:pokedex_assessment/Utils/ApiConstants.dart';
 import 'package:pokedex_assessment/Utils/CustomTextStyling.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,24 +14,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final CarouselController _controller = CarouselController();
 
   late String _valueName;
   late String _valueHeight;
   late String _valueWeight;
-  late String _valueType;
+  late String _valueType1;
+  late String _valueType2;
+  late String _valueImage;
   late int _index;
   late String _detailsUrl;
+  late bool _flag;
 
   @override
   void initState() {
     _valueName = "";
     _valueHeight = "";
     _valueWeight = "";
-    _valueType = "";
+    _valueType1 = "";
+    _valueType2 = "";
+    _valueImage = "";
     _index = 0;
     _detailsUrl = "";
-
-    getItemDetails(_detailsUrl);
+    _flag = true;
 
     super.initState();
   }
@@ -46,19 +52,35 @@ class _HomePageState extends State<HomePage> {
 
     var response = await http.get(url);
     var jsonResponse = convert.jsonDecode(response.body);
-    // print("convertedResponse $jsonResponse");
+
+    setState(() {
+      _detailsUrl = jsonResponse["results"][0]["url"];
+      if (_flag) {
+        getItemDetails(_detailsUrl);
+      }
+    });
+    //print("convertedResponse $jsonResponse");
 
     return jsonResponse;
   }
 
-  getItemDetails(String givenUrl){
-    print("Temp Url $givenUrl");
-    // var tempUrl = Uri.parse(givenUrl);
-    // //print("Url $url");
-    //
-    // var response = await http.get(tempUrl);
-    // var jsonResponse = convert.jsonDecode(response.body);
-    // print(jsonResponse);
+  Future getItemDetails(String detailsUrl) async {
+    var tempUrl = Uri.parse(detailsUrl);
+
+    var response = await http.get(tempUrl);
+    var jsonResponse = convert.jsonDecode(response.body);
+
+    setState(() {
+      _flag = false;
+      _valueName = jsonResponse["name"];
+      _valueHeight = jsonResponse["height"].toString();
+      _valueWeight = jsonResponse["weight"].toString();
+      _valueType1 = jsonResponse["types"][0]["type"]["name"];
+      _valueType2 = jsonResponse["types"][1]["type"]["name"];
+      _valueImage = jsonResponse["sprites"]["front_default"];
+    });
+
+    // print("ItemDetails $jsonResponse");
   }
 
   @override
@@ -84,44 +106,56 @@ class _HomePageState extends State<HomePage> {
               return Column(
                 children: [
                   CarouselSlider(
-                      items: snapshot.data["results"].map<Widget>((item) => Container(
-                            margin: const EdgeInsets.all(1.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    '# ${snapshot.data["results"].indexOf(item) + 1}',
-                                    style: nameTextStyle,
-                                  ),
-                                )
-                              ],
+                    items: snapshot.data["results"]
+                        .map<Widget>(
+                          (item) => Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                            color: Colors.grey,
+                            child: Container(
+                              width: 180,
+                              height: 120,
+                              margin: const EdgeInsets.all(1.0),
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(image: NetworkImage(_valueImage),
+                                      fit: BoxFit.cover)
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      '# ${snapshot.data["results"].indexOf(item) + 1}',
+                                      style: nameTextStyle,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          )).toList(),
-                      options: CarouselOptions(
-                        height: 300,
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.8,
-                        initialPage: 0,
-                        enableInfiniteScroll: true,
-                        reverse: false,
-                        autoPlayCurve: Curves.fastOutSlowIn,
-                        enlargeCenterPage: true,
-                        onPageChanged: (index,reason) {
-                          setState(() {
-                            if(_index != 0){
-                              _index = _index + 1;
-                              _detailsUrl = snapshot.data["results"][_index]["url"];
-                            }else{
-                              _detailsUrl = snapshot.data["results"][_index]["url"];
-                            }
-
-                          });
-                        },
-                        scrollDirection: Axis.horizontal,
-                      )),
+                          ),
+                        )
+                        .toList(),
+                    options: CarouselOptions(
+                      height: 300,
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.5,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      reverse: false,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                      onPageChanged: (position, reason) {
+                        setState(() {
+                          _index = position;
+                          _detailsUrl = snapshot.data["results"][_index]["url"];
+                          getItemDetails(_detailsUrl);
+                        });
+                      },
+                      scrollDirection: Axis.horizontal,
+                    ),
+                    carouselController: _controller,
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
@@ -138,14 +172,14 @@ class _HomePageState extends State<HomePage> {
                     children: [
                       Flexible(
                         child: Text(
-                          _valueHeight,
+                          "Height: $_valueHeight",
                           style: normalTextStyle,
                         ),
                         flex: 1,
                       ),
                       Flexible(
                         child: Text(
-                          _valueWeight,
+                          "Weight: $_valueWeight",
                           style: normalTextStyle,
                         ),
                         flex: 1,
@@ -156,7 +190,7 @@ class _HomePageState extends State<HomePage> {
                     height: 15,
                   ),
                   Text(
-                    _valueType,
+                    "Type: $_valueType1 / $_valueType2",
                     style: normalTextStyle,
                   ),
                 ],
